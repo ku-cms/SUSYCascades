7b5a5ba11f9c6baca427bb35d3b837723cdf96d3
Zachary Flowers <zacharyflowers96@gmail.com>
2025-08-09 10:45:41 -0500

    update EC plot to use ST

--- macros/EventCountPlot.C
+++ macros/EventCountPlot.C
@@ -1,54 +1,4 @@
-#include <iostream>
-#include <string>
-//#include <boost/filesystem.hpp>
-
-#include <TROOT.h>
-#include <TFile.h>
-#include <TTree.h>
-#include <TBranch.h>
-#include <TGraph.h>
-#include <TMultiGraph.h>
-#include <TAxis.h>
-#include <TCanvas.h>
-#include <TMath.h>
-#include <TLegend.h>
-#include <TLatex.h>
-#include <TColor.h>
-#include <TColorWheel.h>
-#include <TH1D.h>
-#include <TStyle.h>
-#include <TLorentzVector.h>
-#include <TSystem.h>
-
-#include "../include/ReducedBase_slim.hh"
-#include "../include/SampleTool.hh"
-#include "../include/CategoryTool.hh"
-#include "../include/ScaleFactorTool.hh"
-#include "../include/Leptonic.hh"
-#include "../include/Hadronic.hh"
-#include "../include/FitReader.hh"
-
-#include "RestFrames/RestFrames.hh"
-
-using namespace std;
-
-// global variables
-string g_PlotTitle;
-string g_Label;
-string g_Xname;
-double g_Xmin;
-double g_Xmax;
-double g_XN;
-string g_Yname;
-double g_Ymin;
-double g_Ymax;
-double g_YN;
-string g_dm_Yname;
-double g_dm_Ymin;
-double g_dm_Ymax;
-double g_dm_YN;
-
-using namespace RestFrames;
+#include "PlottingTools.h"
 
 void format(TH2D* hist, string Xname, string Yname)
 {
@@ -101,300 +51,39 @@ void EventCountPlot()
   
   RestFrames::SetStyle();
 
-  //string NtuplePath = "/home/t3-ku/z374f439/storage/crogan/";
-  string NtuplePath = "root://xrootd.unl.edu//store/user/zflowers/crogan/";
+  string NtuplePath = "/local-scratch/zflowers/NTUPLES/HADD/";
 
-  vector<VS> signals;
-  VS signal_labels;
+  // 10 is Summer23BPix
+  SampleTool ST(NtuplePath, 10);
+  std::vector<std::pair<std::string, ProcessList>> vec_samples;
 
-  VS T2tt;
-  VS T2bW;
-  VS TChiWZ;
-  VS TChipmWW;
-  VS TSlepSlep;
-  VS T2bb;
-  VS T2cc;
-  VS T1bbbb;
-  VS T1ttbb;
-  VS T5tttt;
-  VS TChiWH;
-  
-  // ------------ //
-  // --- 2016 --- // 
-  // ------------ //
-  
-  // TODO: Update T2bW_2016 file paths once files are created
-  VS T2bW_2016;
-  T2bW_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2bW_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2bW_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2bW_X05_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  //T2bW_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2bW_X05_dM-10to80_2Lfilter_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2bW += T2bW_2016;
+  std::map<std::string, VS> map_vsignals;
+  map_vsignals.insert(std::make_pair("T1bbbb", VS({"T1bbbb_*"})));
+  // loop over signals and add to map
+  for(auto p = map_vsignals.begin(); p != map_vsignals.end(); p++){
+    ProcessList signals;
+    for(auto s : p->second){
+      signals += ST.Get(s);
+    }
+    vec_samples.push_back(std::make_pair(p->first, signals));
+  }
 
-  VS T2tt_2016;
-  T2tt_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2tt_dM-10to80_genHT-160_genMET-80_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2tt_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2tt_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2tt_mStop-400to1200_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2tt_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2tt_dM-6to8_genHT-160_genMET-80_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  //T2tt_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2tt_dM-10to80_2Lfilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2tt += T2tt_2016;
-
-  VS TChiWZ_2016;
-  //TChiWZ_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChiWZ_ZToLL_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  //TChiWZ_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChiWZ_ZToLL_dM-90to100_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  //TChiWZ_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TChiWZ_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChiWZ_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TChiWZ_2016.a(NtuplePath+"Summer16_102X_SMS/TChiWZ_genHT-160_genMET-80_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TChiWZ += TChiWZ_2016;
-
-  VS TChipmWW_2016;
-  TChipmWW_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChipmWW_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  //TChipmWW_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChipmWW_WWTo2LNu_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TChipmWW_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TChipmWW_dM-3to50_genHT-160_genMET-80_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TChipmWW += TChipmWW_2016;
-
-  VS TSlepSlep_2016;
-  TSlepSlep_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TSlepSlep_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TSlepSlep_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TSlepSlep_TuneCP2_13TeV-madgraphMLM-pythia8_ext_Summer16_102X.root");
-  TSlepSlep_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-TSlepSlep_mSlep-500To1300_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  TSlepSlep += TSlepSlep_2016;
-  
-  // TODO: Update T2bb_2016 file paths once files are created
-  VS T2bb_2016;
-  T2bb_2016.a(NtuplePath+"Summer16_102X_SMS/SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8_Summer16_102X.root");
-  T2bb += T2bb_2016;
-  
-  // ------------ //
-  // --- 2017 --- // 
-  // ------------ //
-
-  VS T2bW_2017;
-  T2bW_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2bW_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2bW_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2bW_X05_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  //T2bW_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2bW_X05_dM-10to80_2Lfilter_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2bW += T2bW_2017;
-  
-  VS T2tt_2017;
-  T2tt_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2tt_dM-10to80_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2tt_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2tt_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2tt_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2tt_dM-6to8_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  //T2tt_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2tt_dM-10to80_2Lfilter_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2tt += T2tt_2017;
-
-  VS TChiWZ_2017;
-  //TChiWZ_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChiWZ_ZToLL_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  //TChiWZ_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChiWZ_ZToLL_dM-90to100_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  //TChiWZ_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  //TChiWZ_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChiWZ_ZToLL_mZMin-0p1_mC1-325to1000_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TChiWZ_2017.a(NtuplePath+"Fall17_102X_SMS/TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TChiWZ += TChiWZ_2017;
-
-  VS TChipmWW_2017;
-  TChipmWW_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChipmWW_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  //TChipmWW_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChipmWW_WWTo2LNu_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TChipmWW_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChipmWW_dM-3to50_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TChipmWW += TChipmWW_2017;
-
-  VS TSlepSlep_2017;
-  TSlepSlep_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TSlepSlep_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TSlepSlep_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TSlepSlep_TuneCP2_13TeV-madgraphMLM-pythia8_ext_Fall17_102X.root");
-  TSlepSlep_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TSlepSlep_mSlep-500To1300_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TSlepSlep += TSlepSlep_2017;
-
-  VS T2bb_2017;
-  T2bb_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2bb += T2bb_2017;
-
-  VS T2cc_2017;
-  T2cc_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T2cc_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T2cc += T2cc_2017;
-
-  VS T1bbbb_2017;
-  T1bbbb_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T1bbbb_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T1bbbb += T1bbbb_2017;
-
-  VS T1ttbb_2017;
-  T1ttbb_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T1ttbb_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T1ttbb += T1ttbb_2017;
-
-  VS T5tttt_2017;
-  T5tttt_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-T5tttt_dM175_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  T5tttt += T5tttt_2017;
-
-  VS TChiWH_2017;
-  TChiWH_2017.a(NtuplePath+"Fall17_102X_SMS/SMS-TChiWH_WToLNu_HToBB_TuneCP2_13TeV-madgraphMLM-pythia8_Fall17_102X.root");
-  TChiWH += TChiWH_2017;
-  
-  // ------------ //
-  // --- 2018 --- // 
-  // ------------ //
- 
-  // TODO: Update T2bW_2018 file paths once files are created
-  VS T2bW_2018;
-  T2bW_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2bW_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2bW_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2bW_X05_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  //T2bW_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2bW_X05_dM-10to80_2Lfilter_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2bW += T2bW_2018;
-  
-  VS T2tt_2018;
-  T2tt_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2tt_dM-10to80_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2tt_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2tt_dM-10to80_genHT-160_genMET-80_mWMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2tt_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2tt_mStop-400to1200_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2tt_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2tt_dM-6to8_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  //T2tt_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2tt_dM-10to80_2Lfilter_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2tt += T2tt_2018;
-
-  VS TChiWZ_2018;
-  //TChiWZ_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChiWZ_ZToLL_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  //TChiWZ_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChiWZ_ZToLL_dM-90to100_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  //TChiWZ_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChiWZ_ZToLL_mZMin-0p1_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  //TChiWZ_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChiWZ_ZToLL_mZMin-0p1_mC1-325to1000_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TChiWZ_2018.a(NtuplePath+"Autumn18_102X_SMS/TChiWZ_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TChiWZ += TChiWZ_2018;
-
-  VS TChipmWW_2018;
-  TChipmWW_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChipmWW_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  //TChipmWW_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChipmWW_WWTo2LNu_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TChipmWW_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChipmWW_dM-3to50_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TChipmWW += TChipmWW_2018;
-
-  VS TSlepSlep_2018;
-  TSlepSlep_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TSlepSlep_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TSlepSlep_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TSlepSlep_TuneCP2_13TeV-madgraphMLM-pythia8_ext_Autumn18_102X.root");
-  TSlepSlep_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TSlepSlep_mSlep-500To1300_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TSlepSlep += TSlepSlep_2018;
-
-  // TODO: Update T2bb_2018 file paths once files are created
-  VS T2bb_2018;
-  T2bb_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2bb_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2bb += T2bb_2018;
-
-  VS T2cc_2018;
-  T2cc_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T2cc_genHT-160_genMET-80_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T2cc += T2cc_2018;
-
-  VS T1bbbb_2018;
-  T1bbbb_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T1bbbb_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T1bbbb += T1bbbb_2018;
-
-  VS T1ttbb_2018;
-  T1ttbb_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T1ttbb_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T1ttbb += T1ttbb_2018;
-
-  VS T5tttt_2018;
-  T5tttt_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-T5tttt_dM175_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  T5tttt += T5tttt_2018;
-
-  VS TChiWH_2018;
-  TChiWH_2018.a(NtuplePath+"Autumn18_102X_SMS/SMS-TChiWH_WToLNu_HToBB_TuneCP2_13TeV-madgraphMLM-pythia8_Autumn18_102X.root");
-  TChiWH += TChiWH_2018;
-
-  // --- 2016 --- // 
-  signal_labels.a("T2bW_2016");
-  signals.push_back(T2bW_2016);
-  //signal_labels.a("T2tt_2016");
-  //signals.push_back(T2tt_2016);
-  //signal_labels.a("TChiWZ_2016");
-  //signals.push_back(TChiWZ_2016);
-  //signal_labels.a("TChipmWW_2016");
-  //signals.push_back(TChipmWW_2016);
-  //signal_labels.a("TSlepSlep_2016");
-  //signals.push_back(TSlepSlep_2016);
-  //signal_labels.a("T2bb_2016");
-  //signals.push_back(T2bb_2016);
-  
-  // --- 2017 --- //
-  signal_labels.a("T2bW_2017");
-  signals.push_back(T2bW_2017);
-  //signal_labels.a("T2tt_2017");
-  //signals.push_back(T2tt_2017);
-  //signal_labels.a("TChiWZ_2017");
-  //signals.push_back(TChiWZ_2017);
-  //signal_labels.a("TChipmWW_2017");
-  //signals.push_back(TChipmWW_2017);
-  //signal_labels.a("TSlepSlep_2017");
-  //signals.push_back(TSlepSlep_2017);
-  //signal_labels.a("T2bb_2017");
-  //signals.push_back(T2bb_2017);
-  //signal_labels.a("T2cc_2017");
-  //signals.push_back(T2cc_2017);
-  //signal_labels.a("T1bbbb_2017");
-  //signals.push_back(T1bbbb_2017);
-  //signal_labels.a("T1ttbb_2017");
-  //signals.push_back(T1ttbb_2017);
-  //signal_labels.a("T5tttt_2017");
-  //signals.push_back(T5tttt_2017);
-  //signal_labels.a("TChiWH_2017");
-  //signals.push_back(TChiWH_2017);
-  
-  // --- 2018 --- //
-  signal_labels.a("T2bW_2018");
-  signals.push_back(T2bW_2018);
-  //signal_labels.a("T2tt_2018");
-  //signals.push_back(T2tt_2018);
-  //signal_labels.a("TChiWZ_2018");
-  //signals.push_back(TChiWZ_2018);
-  //signal_labels.a("TChipmWW_2018");
-  //signals.push_back(TChipmWW_2018);
-  //signal_labels.a("TSlepSlep_2018");
-  //signals.push_back(TSlepSlep_2018);
-  //signal_labels.a("T2bb_2018");
-  //signals.push_back(T2bb_2018);
-  //signal_labels.a("T2cc_2018");
-  //signals.push_back(T2cc_2018);
-  //signal_labels.a("T1bbbb_2018");
-  //signals.push_back(T1bbbb_2018);
-  //signal_labels.a("T1ttbb_2018");
-  //signals.push_back(T1ttbb_2018);
-  //signal_labels.a("T5tttt_2018");
-  //signals.push_back(T5tttt_2018);
-  //signal_labels.a("TChiWH_2018");
-  //signals.push_back(TChiWH_2018);
-
-  // --- Run 2 --- //
-  signal_labels.a("T2bW");
-  signals.push_back(T2bW);
-  //signal_labels.a("T2tt");
-  //signals.push_back(T2tt);
-  //signal_labels.a("TChiWZ");
-  //signals.push_back(TChiWZ);
-  //signal_labels.a("TChipmWW");
-  //signals.push_back(TChipmWW);
-  //signal_labels.a("TSlepSlep");
-  //signals.push_back(TSlepSlep);
-  //signal_labels.a("T2bb");
-  //signals.push_back(T2bb);
-  //signal_labels.a("T2cc");
-  //signals.push_back(T2cc);
-  //signal_labels.a("T1bbbb");
-  //signals.push_back(T1bbbb);
-  //signal_labels.a("T1ttbb");
-  //signals.push_back(T1ttbb);
-  //signal_labels.a("T5tttt");
-  //signals.push_back(T5tttt);
-  //signal_labels.a("TChiWH");
-  //signals.push_back(TChiWH);
-  
-  for(int s = 0; s < int(signals.size()); s++)
-  {
-    g_Label = signal_labels[s];
-    
+  for (auto p = vec_samples.begin(); p != vec_samples.end(); p++){
+    g_Label = p->first;
     g_Xname = "M_{NLSP} [GeV]";
-    g_Xmin  = 0.;
-    g_Xmax  = 1500.; 
-    g_XN    = 60;
-    //g_XN  = 30;
+    g_Xmin  = 500.;
+    g_Xmax  = 3000.; 
+    g_NX    = 60;
     
     g_Yname = "M_{LSP} [GeV]";
-    g_Ymin  = 0.;
-    g_Ymax  = 1000.;
-    g_YN    = 40;
-    //g_YN  = 20;
+    g_Ymin  = 500.;
+    g_Ymax  = 2000.;
+    g_NY    = 40;
 
     // mass diff plot
     g_dm_Yname = "M_{NLSP} - M_{LSP} [GeV]";
     g_dm_Ymin  = 0.;
-    g_dm_Ymax  = 200.;
+    g_dm_Ymax  = 1600.;
     g_dm_YN    = 20;
     
     // Change label for T2bW
@@ -410,38 +99,31 @@ void EventCountPlot()
     TH2D* hist = new TH2D(
       (g_Label+"_EventCount").c_str(),
       (g_Label+"_EventCount").c_str(),
-      g_XN, g_Xmin, g_Xmax,
-      g_YN, g_Ymin, g_Ymax
+      g_NX, g_Xmin, g_Xmax,
+      g_NY, g_Ymin, g_Ymax
     );
     // mass diff (dm) vs m_nlsp
     TH2D* hist_dm = new TH2D(
       (g_Label+"_dm_EventCount").c_str(),
       (g_Label+"_dm_EventCount").c_str(),
-      g_XN,    g_Xmin,    g_Xmax,
+      g_NX,    g_Xmin,    g_Xmax,
       g_dm_YN, g_dm_Ymin, g_dm_Ymax
     );
     
-    //cout << "Processing " << Nfile << " files for process " << title << endl;
-    for(int f = 0; f < int(signals[s].size()); f++)
-    {
-      vector<string> tree_names;
-      //TFile* file = new TFile(signals[s][f].c_str(),"READ");
-      TFile* file = TFile::Open(signals[s][f].c_str(),"READ");
-      if(file == 0 || file->IsZombie()) { cout << "Cannot open: " << signals[s][f] << endl; continue; }
-      for (auto&& keyAsObj : *file->GetListOfKeys())
-      {
-        auto key = (TKey*) keyAsObj;
-        string name = key->GetName();
-        if (name.find("SMS") == string::npos) continue;
-        tree_names.push_back(key->GetName());
-      }
-      for(int k = 0; k < int(tree_names.size()); k++)
-      {
-        TChain* chain = new TChain(tree_names[k].c_str());
-        chain->Add(signals[s][f].c_str());
-        int events = chain->GetEntries(); 
+    int Nsample = p->second.GetN();
+    for(int s = 0; s < Nsample; s++){
+      Process proc = p->second[s];
+      int Nfile = ST.NTrees(proc);
+      for(int f = 0; f < Nfile; f++){
+        string file = ST.FileName(proc, f);
+        string tree = ST.TreeName(proc, f);
+        TChain* chain = ST.Tree(proc, f);
+        ReducedBase_V2* base = new ReducedBase_V2(chain);
+        gErrorIgnoreLevel = kFatal;
+        int events = base->fChain->GetEntries(); 
+        gErrorIgnoreLevel = 0;
         delete chain;
-        string NLSP_Mass_string = tree_names[k].erase(0,4); //remove SMS_
+        string NLSP_Mass_string = tree.erase(0,4); //remove SMS_
         string LSP_Mass_string = NLSP_Mass_string;
         NLSP_Mass_string = NLSP_Mass_string.substr(0,NLSP_Mass_string.find("_"));
         double NLSP_Mass = std::stod(NLSP_Mass_string);
@@ -451,7 +133,6 @@ void EventCountPlot()
         hist->Fill(NLSP_Mass, LSP_Mass, events);
         hist_dm->Fill(NLSP_Mass, NLSP_Mass - LSP_Mass, events);
       }
-      file->Close();
     }
 
     cout << "Total " << hist->Integral() << endl;
@@ -502,4 +183,5 @@ void EventCountPlot()
     
     delete can;
   }
+  gApplication->Terminate(0);
 }
