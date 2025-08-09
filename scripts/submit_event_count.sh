13a617909308a3160dbe24d5d340e1754bd4b6df
Zachary Flowers <zacharyflowers96@gmail.com>
2025-08-09 10:49:41 -0500

    update EC main script to auto compile and run checker

--- scripts/submit_event_count.sh
+++ scripts/submit_event_count.sh
@@ -1,3 +1,6 @@
+# Use this script to submit ALL EC jobs
+make clean
+make cmssw -j 8
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer16_102X.list
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer16_102X_SMS.list --sms
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Fall17_102X.list
@@ -16,3 +19,5 @@ python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer23BPix_130X_Cascades.list --cascades
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer22_130X_Cascades.list --cascades --privateMC
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer22_130X_SMS.list --sms --privateMC
+# Run checker after submitting all jobs
+nohup python3 python/DASCheck_EventCount.py -d > /dev/null 2>&1 &
