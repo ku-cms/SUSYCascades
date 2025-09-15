5f487283a8203e9e94b4269a1d9cfe6865fc69aa
Zachary Flowers <zacharyflowers96@gmail.com>
2025-09-15 12:33:19 -0500

    add UL sms lists and add to EC

--- scripts/submit_event_count.sh
+++ scripts/submit_event_count.sh
@@ -8,9 +8,13 @@ python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Autumn18_102X.list
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Autumn18_102X_SMS.list --sms
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL16_106X.list
+python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL16_106X_SMS.list --sms
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL16APV_106X.list
+python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL16APV_106X_SMS.list --sms
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL17_106X.list
+python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL17_106X_SMS.list --sms
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL18_106X.list
+python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer20UL18_106X_SMS.list --sms
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer22_130X.list
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer22EE_130X.list
 python3 scripts/condor_submit_nano_cmslpc_eventCount.py --connect -list samples/NANO/Lists/Summer23_130X.list
