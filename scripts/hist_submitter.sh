852d1eedea3f8596ef5bcd49a962899cd097f9f0
Zachary Flowers <zacharyflowers96@gmail.com>
2026-04-13 11:35:08 -0500

    update hist scripts

--- scripts/hist_submitter.sh
+++ scripts/hist_submitter.sh
@@ -1,13 +1,5 @@
 make clean
 make cmssw -j 8
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL16APV_106X.list --verbose
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL16_106X.list --verbose
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL17_106X.list --verbose
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL18_106X.list --verbose
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer22_130X.list --verbose --privateMC
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer22EE_130X.list --verbose --privateMC
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer23_130X.list --verbose --privateMC
-python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer23BPix_130X.list --verbose --privateMC
 python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL16APV_106X_SMS.list --verbose --sms
 python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL16_106X_SMS.list --verbose --sms
 python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL17_106X_SMS.list --verbose --sms
@@ -15,3 +7,11 @@ python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/List
 python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer22_130X_SMS.list --verbose --sms --privateMC
 python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer22_130X_Cascades.list --verbose --cascades --privateMC
 python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer23BPix_130X_Cascades.list --verbose --cascades
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL16APV_106X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL16_106X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL17_106X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer20UL18_106X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer22_130X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer22EE_130X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer23_130X.list --verbose
+python3 scripts/condor_submit_nano_connect_histograms.py -list samples/NANO/Lists/Summer23BPix_130X.list --verbose
