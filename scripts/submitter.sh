f21568f51cd8f60797bb37b03a8c6ac1b1d7ce78
Zachary Flowers <zacharyflowers96@gmail.com>
2026-05-04 16:41:29 -0500

    update submitter script

--- scripts/submitter.sh
+++ scripts/submitter.sh
@@ -2,14 +2,15 @@ python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/S
 python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL16_106X_SMS.list --verbose --sms --sys --slim
 python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL17_106X_SMS.list --verbose --sms --sys --slim
 python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL18_106X_SMS.list --verbose --sms --sys --slim
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL16APV_106X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL16_106X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL17_106X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer20UL18_106X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer22_130X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer22EE_130X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer23_130X_Data.list --verbose --data
-python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer23BPix_130X_Data.list --verbose --data
 python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer23BPix_130X_Cascades.list --verbose --cascades --sys --slim
 python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer22_130X_Cascades.list --verbose --cascades --privateMC --sys --slim
 python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Summer22_130X_SMS.list --verbose --sms --privateMC --sys --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer20UL16APV_106X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer20UL16_106X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer20UL17_106X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer20UL18_106X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer22_130X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer22EE_130X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer23_130X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer23BPix_130X_Data_MET.list --verbose --data --slim
+python3 scripts/condor_submit_nano_connect_ntuples.py -list samples/NANO/Lists/Additional_Lists/Summer24_130X_Data_MET.list --verbose --data --slim
